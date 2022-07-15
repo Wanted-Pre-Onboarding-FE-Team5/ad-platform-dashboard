694c9857ae751ce485cb316fe918a95278de07a7
ymStudyLog <ylee585@gmail.com>
2022-07-15 10:37:10 +0900

    Modify : database.json 위치 public으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTotalAdStatusModel } from "../src/models/useTotalAdStatusModel";
 
-//렌더링 되는데 이상하게 새로고침하면 사라짐. 안보임..?
+//렌더링 되는데 이상하게 새로고침하면 사라짐. 안보임..? useEffect 로직 고쳐야될거 같음...
 
 type TotalReportDataType = {
   //total-report 데이터 타입
@@ -20,38 +20,36 @@ type TotalReportDataType = {
 
 const App = () => {
   const { totalAdStatus, getTotalAdStatus } = useTotalAdStatusModel();
-  const [adStatus, setAdStatus] = React.useState([]);
+  const [dailyAdStatus, setDailyAdStatus] = React.useState <Array<TotalReportDataType>|null>([]); 
 
   React.useEffect(() => {
     getTotalAdStatus();
-    if (totalAdStatus === null) setAdStatus([]);
-    else setAdStatus(totalAdStatus["daily"]);
+    setDailyAdStatus(totalAdStatus === null? [] : totalAdStatus["daily"]);
   }, []);
 
   console.log(totalAdStatus === null ? [] : totalAdStatus["daily"]);
 
-  // map함수로 배열을 새로 생성한 후 return으로 컴포넌트를 반환할 수 있습니다.
-  const adStatusList: JSX.Element[] = adStatus?.map(
-    (ad: TotalReportDataType, index:number) => {
+  const dailyAdStatusList = dailyAdStatus?.map(
+    (dailyAd , index:number) => {
       return (
         <ul key={index}>
-          <li key={ad.imp}>{ad.imp}</li>
-          <li key={ad.click}>{ad.click}</li>
-          <li key={ad.cost}>{ad.cost}</li>
-          <li key={ad.conv}>{ad.conv}</li>
-          <li key={ad.convValue}>{ad.convValue}</li>
-          <li key={ad.ctr}>{ad.ctr}</li>
-          <li key={ad.cvr}>{ad.cvr}</li>
-          <li key={ad.cpc}>{ad.cpc}</li>
-          <li key={ad.cpa}>{ad.cpa}</li>
-          <li key={ad.roas}>{ad.roas}</li>
-          <li key={ad.date}>{ad.date}</li>
+          <li key={dailyAd.imp}>imp: {dailyAd.imp}</li>
+          <li key={dailyAd.click}>click: {dailyAd.click}</li>
+          <li key={dailyAd.cost}>cost: {dailyAd.cost}</li>
+          <li key={dailyAd.conv}>conv: {dailyAd.conv}</li>
+          <li key={dailyAd.convValue}>convValue: {dailyAd.convValue}</li>
+          <li key={dailyAd.ctr}>ctr: {dailyAd.ctr}</li>
+          <li key={dailyAd.cvr}>cvr: {dailyAd.cvr}</li>
+          <li key={dailyAd.cpc}>cpc: {dailyAd.cpc}</li>
+          <li key={dailyAd.cpa}>cpa: {dailyAd.cpa}</li>
+          <li key={dailyAd.roas}>roas: {dailyAd.roas}</li>
+          <li key={dailyAd.date}>date: {dailyAd.date}</li>
         </ul>
       );
     }
   ); 
 
-  return <div>{adStatusList}</div>;
+  return <div>{dailyAdStatusList}</div>;
 };
 
 export default App;
