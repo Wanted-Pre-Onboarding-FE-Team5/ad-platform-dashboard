3fa70898c4d8dee97fcbf47418a1405893a7df83
yunjink <chloeeekim1109@gmail.com>
2022-07-18 14:33:05 +0900

    modify : Header css 수정

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Box, Toolbar } from "@mui/material";
-import TotalAdStatus from "../components/TotalAdStatus";
-import ChannelStatus from "../components/ChannelStatus";
+import React from 'react';
+import { Box, Toolbar } from '@mui/material';
+import TotalAdStatus from '../components/TotalAdStatus';
+import ChannelStatus from '../components/ChannelStatus';
 
 const Dashboard = () => {
   return (
     <Box
-      component="main"
+      component='main'
       sx={{
         flexGrow: 1,
-        bgcolor: "#f5f5f5",
-        mt: "4rem",
+        bgcolor: '#f5f5f5',
+        mt: '4rem',
         width: `calc(100vw - 240px)`,
       }}
     >
-      <Toolbar>대시 보드</Toolbar>
+      <Toolbar sx={{ color: '#59656b', fontSize: '30px', fontWeight: 'bold' }}>대시 보드</Toolbar>
       <TotalAdStatus />
       <ChannelStatus />
     </Box>
