4fa697249d5cde834bfc97f7364cecd5cb5455ce
yunjink <chloeeekim1109@gmail.com>
2022-07-20 13:44:16 +0900

    feat : StackedBarChart, Table 수정

--- src/components/dashboard/channelStatus/ChannelStatus.tsx
+++ src/components/dashboard/channelStatus/ChannelStatus.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import StackedBarChart from './StackedBarChart';
 import BasicTable from './BasicTable';
-import { ChannelStatusType } from "../../../models/types";
-import { dataService, getChannelStatusData } from '../../../api/api';
 import { Box, Container, Typography } from '@mui/material';
 
 const ChannelStatus = () => {
-  const [channelState, setChannelState] = React.useState<ChannelStatusType[]>();
-
-  React.useEffect(() => {
-    getChannelStatusData(dataService("channelStatus"),"")
-    .then((data) => setChannelState(data))
-    .catch(() => console.log("data dispatch error"))
-  }, []);
-  console.log(channelState);
-
   return (
     <Box sx={{ p: 3 }}>
       <Typography sx={{ mb: 3, fontWeight: 'bold' }}>매체 현황</Typography>
