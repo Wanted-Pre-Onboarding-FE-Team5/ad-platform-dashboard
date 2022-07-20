c4c8fba455aca5c445ac0a859f2b086ec5b6a85c
yunjink <chloeeekim1109@gmail.com>
2022-07-20 16:16:20 +0900

    feat : StackedBarChart 수정 중

--- src/components/dashboard/channelStatus/StackedBarChart.tsx
+++ src/components/dashboard/channelStatus/StackedBarChart.tsx
@@ -5,36 +5,53 @@ import { ChannelStatusType } from '../../../models/types';
 import { dataService, getChannelStatusData } from '../../../api/api';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-// const data = [
-//   {
-//     name: 'Page A',
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: 'Page B',
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: 'Page C',
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-// ];
+const data = [
+  {
+    name: '광고비',
+    페이스북: 4000,
+    네이버: 2400,
+    구글: 2400,
+    카카오: 3000,
+  },
+  {
+    name: '매출',
+    페이스북: 4000,
+    네이버: 2400,
+    구글: 2400,
+    카카오: 3000,
+  },
+  {
+    name: '노출수',
+    페이스북: 4000,
+    네이버: 2400,
+    구글: 2400,
+    카카오: 3000,
+  },
+  {
+    name: '클릭수',
+    페이스북: 4000,
+    네이버: 2400,
+    구글: 2400,
+    카카오: 3000,
+  },
+  {
+    name: '전환수',
+    페이스북: 4000,
+    네이버: 2400,
+    구글: 2400,
+    카카오: 3000,
+  },
+];
 
 const StackedBarChart = () => {
-  const [channelStatus, setChannelStatus] = useRecoilState<ChannelStatusType[]>(channelState);
+  // const [channelStatus, setChannelStatus] = useRecoilState<ChannelStatusType[]>(channelState);
 
-  React.useEffect(() => {
-    getChannelStatusData(dataService('channelStatus'), '')
-      .then((data) => setChannelStatus(data))
-      .catch(() => console.log('data dispatch error'));
-  }, [setChannelStatus]);
-  console.log(channelStatus);
+  // React.useEffect(() => {
+  //   getChannelStatusData(dataService('channelStatus'), '')
+  //     .then((data) => setChannelStatus(data))
+  //     .catch(() => console.log('data dispatch error'));
+  // }, [setChannelStatus]);
+  // console.log(channelStatus);
 
   return (
     <div style={{ width: '100%', height: 400 }}>
@@ -42,23 +59,24 @@ const StackedBarChart = () => {
         <BarChart
           width={900}
           height={400}
-          data={channelStatus}
+          data={data}
           margin={{
             top: 20,
             right: 30,
             left: 20,
-            bottom: 5,
+            bottom: 20,
           }}
         >
           {/* <CartesianGrid strokeDasharray='1 0' /> */}
           {/* TODO X축이 각 매체의 광고비, 매출, 노출수, 클릭 수, 전환수 가 되게 하기*/}
-          <XAxis dataKey='cost' />
+          <XAxis dataKey='name' />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey='pv' stackId='a' fill='#4fadf7' />
-          <Bar dataKey='uv' stackId='a' fill='#85da47' />
-          <Bar dataKey='amt' stackId='a' fill='#ac8af8' />
+          <Bar dataKey='페이스북' stackId='a' fill='#4fadf7' />
+          <Bar dataKey='네이버' stackId='a' fill='#85da47' />
+          <Bar dataKey='구글' stackId='a' fill='#ac8af8' />
+          <Bar dataKey='카카오' stackId='a' fill='#f7d500' />
         </BarChart>
       </ResponsiveContainer>
     </div>
